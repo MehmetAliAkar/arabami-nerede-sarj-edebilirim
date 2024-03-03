0c1f0b8efd7baff0e13cf78fc80f5d1785448cd2
Mehmet Ali AKAR <maliakar123@gmail.com>
2024-03-03 20:38:11 +0300

    init project

---
+++ src/main/java/com/mehmetaliakar/istasyonumnerede/IstasyonumNeredeApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetaliakar.istasyonumnerede;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstasyonumNeredeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstasyonumNeredeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mehmetaliakar/istasyonumnerede/IstasyonumNeredeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetaliakar.istasyonumnerede;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstasyonumNeredeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
